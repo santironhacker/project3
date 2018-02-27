9e90b83e31df913b6b65c486de53c1d50b57da57
Santi Ramos <santi92.network@gmail.com>
2018-02-27 19:42:10 +0100

    set auth guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 // Services
+import { AuthService } from './services/auth.service';
+
+// -- guards
+import { RequireAnonGuardService } from './services/guards/require-anon-guard.service';
+import { RequireUserGuardService } from './services/guards/require-user-guard.service';
+import { InitAuthGuardService } from './services/guards/init-auth-guard.service';
 
 // Pages
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
@@ -15,11 +22,15 @@ import { LoginComponent } from './components/login/login.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
-
+// -- routes
 const routes: Routes = [
-  { path: '', component: LandingPageComponent }
-  // { path: 'restaurants/:id', component: RestaurantDetailComponent }
+  { path: '',  component: LandingPageComponent, canActivate: [ InitAuthGuardService ] },
+  { path: 'login',  component: LoginComponent, canActivate: [ RequireAnonGuardService ] },
+  { path: 'signup',  component: SignUpComponent, canActivate: [ RequireAnonGuardService ] },
+  // { path: 'page',  component: ... , canActivate: [ RequireUserGuardService ] },
+  { path: '**', redirectTo: '' }
 ];
+
 
 @NgModule({
   declarations: [
@@ -30,10 +41,12 @@ const routes: Routes = [
     LayoutPageComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     RouterModule.forRoot(routes)
+
   ],
-  providers: [],
+  providers: [AuthService, InitAuthGuardService, RequireAnonGuardService, RequireUserGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
