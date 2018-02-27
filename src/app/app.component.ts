5da112706c592709e3d6436bbe9d2fc5ffee2a30
Santi Ramos <santi92.network@gmail.com>
2018-02-27 18:28:47 +0100

    set auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  loading = true;
+  anon: boolean;
+  user: any;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.authService.userChange$.subscribe((user) => {
+      this.loading = false;
+      this.user = user;
+      this.anon = !user;
+    });
+  }
+
+  logout() {
+    this.authService.logout()
+      .then(() => this.router.navigate(['/login']));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
+// Routers and modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 
+// Services
+
+// Pages
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+
+// Components
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+
+
+
 const routes: Routes = [
-  { path: '', component: AppComponent }
+  { path: '', component: LandingPageComponent }
   // { path: 'restaurants/:id', component: RestaurantDetailComponent }
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    SignUpComponent,
+    LoginComponent,
+    LayoutPageComponent
   ],
   imports: [
     BrowserModule,
