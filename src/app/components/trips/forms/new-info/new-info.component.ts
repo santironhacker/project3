ef5c4ad795c1ed7c78b9c561b7e403f669d70144
stepsen89 <stephanie.senoner@gmail.com>
2018-02-28 20:54:46 +0100

    form create new trip - data binding input output in progress

--- src/app/components/trips/forms/new-info/new-info.component.ts
+++ src/app/components/trips/forms/new-info/new-info.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TripService } from '../../../../services/trip.service';
 import { Router } from '@angular/router';
+// import { EventEmitter } from 'events';
 
 
 
@@ -21,31 +22,35 @@ export class NewInfoComponent implements OnInit {
   // name
   // description
   //in the template name description
+  @Output() trip2 = new EventEmitter<any>();
 
   constructor(private tripService: TripService, private router: Router) { }
 
   ngOnInit() {
   }
 
-  submitForm(form) {
-    this.error = '';
-    this.feedbackEnabled = true;
-    if (form.valid) {
-      this.processing = true;
-      this.tripService.createTrip({
-          name: this.trip.name,
-          description: this.trip.description
-      })
-        .then((result) => {
-          this.router.navigate(['/trip/'${id});
-        })
-        .catch((err) => {
-          this.error = err.error.error;
-          this.processing = false;
-          this.feedbackEnabled = false;
-        });
-    }
+  handleSubmitForm(){
+    console.log( this.trip.name, this.trip.description)
   }
+  // submitForm(form) {
+  //   this.error = '';
+  //   this.feedbackEnabled = true;
+  //   if (form.valid) {
+  //     this.processing = true;
+  //     this.tripService.createTrip({
+  //         name: this.trip.name,
+  //         description: this.trip.description
+  //     })
+  //       .then((result) => {
+  //         this.router.navigate(['/trip/'${id});
+  //       })
+  //       .catch((err) => {
+  //         this.error = err.error.error;
+  //         this.processing = false;
+  //         this.feedbackEnabled = false;
+  //       });
+  //   }
+  // }
 
   // when you submit you pas {name, description}
 }
