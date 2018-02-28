561d55fb77633441c3bf5d388e03f912dad77129
stepsen89 <stephanie.senoner@gmail.com>
2018-02-28 18:16:51 +0100

    logout button and redirecting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,6 @@ export class AppComponent implements OnInit {
 
   logout() {
     this.authService.logout()
-      .then(() => this.router.navigate(['/login']));
+      .then(() => this.router.navigate(['/']));
   }
 }
