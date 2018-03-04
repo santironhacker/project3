9067a6116d3ac6770346d018d213c32a48749fdf
stepsen89 <stephanie.senoner@gmail.com>
2018-03-04 23:23:43 +0100

    places information lat lng name (pic) from google object

--- src/app/components/trips/forms/add-place/add-place.component.ts
+++ src/app/components/trips/forms/add-place/add-place.component.ts
@@ -11,12 +11,28 @@ import { MapsAPILoader } from '@agm/core';
 
 
 export class AddPlaceComponent implements OnInit {
-  
   public latitude: number;
   public longitude: number;
   public searchControl: FormControl;
   public zoom: number;
-  
+  lat: number;
+  lng: number;
+  name: String;
+  description: String;
+
+
+  // const placeSchema = new Schema({
+  //   name: String,
+  //   description: String,
+  //   location: {
+  //     type: {
+  //       type: String,
+  //       default: 'Point'
+  //     },
+  //     coordinates: [Number]
+  //   }
+  // });
+
   @ViewChild("search")
   public searchElementRef: ElementRef;
   
@@ -50,7 +66,14 @@ export class AddPlaceComponent implements OnInit {
           if (place.geometry === undefined || place.geometry === null) {
             return;
           }
+          console.log(place.photos[0], place.name);
+          console.log(place.geometry.location.lat());
+          this.lat = place.geometry.location.lat();
+          this.name = place.name;
+          this.lng = place.geometry.location.lng();
 
+          console.log(this.lat);
+          
           //set latitude, longitude and zoom
           this.latitude = place.geometry.location.lat();
           this.longitude = place.geometry.location.lng();
@@ -59,7 +82,7 @@ export class AddPlaceComponent implements OnInit {
       });
     });
   }
-
+  
 
   private setCurrentPosition() {
     if ("geolocation" in navigator) {
@@ -70,4 +93,8 @@ export class AddPlaceComponent implements OnInit {
       });
     }
   }
+
+  addPlace(event){
+  }
+
 }
