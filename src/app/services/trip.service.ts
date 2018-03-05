7755b7eac883d93441274cae238f9d08bb621237
stepsen89 <stephanie.senoner@gmail.com>
2018-03-05 12:45:12 +0100

    deploying heroku environment frontend

--- src/app/services/trip.service.ts
+++ src/app/services/trip.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class TripService {
-
-  API_URL = 'http://localhost:3000/trips';
-
+  
+  API_URL = environment.apiUrl + '/trips';
+  
   constructor(private httpClient: HttpClient) { }
-
+  
   getOne(id): Promise<any> {
     return this.httpClient.get(`${this.API_URL}/${id}`)
     .toPromise()
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://proj3-ih-server.herokuapp.com'
 };
+
